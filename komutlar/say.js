bce4ea51995700293245c3149219f37aa2330a58
Glitch (v12publicvideokobs) <none>
2020-10-04 17:59:43 +0000

    ☂️📊 Checkpoint ./komutlar/say.js:14097343/1582 ./komutlar/kız.js:14097343/7

--- komutlar/say.js
+++ komutlar/say.js
@@ -2,7 +2,13 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
 
-    
+var tagdakiler = 0;
+  let tag = "Kobs";
+message.guild.members.cache.forEach(member => {
+    if(member.user.username.includes(tag)) {
+      tagdakiler = tagdakiler+1
+    }
+  })    
 const voiceChannels = message.guild.channels.cache.filter(c => c.type === 'voice');
     let count = 0
      let botlar = message.guild.members.cache.filter(m => m.user.bot).size;
@@ -10,14 +16,15 @@ const voiceChannels = message.guild.channels.cache.filter(c => c.type === 'voice
     for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
   let  çevrimiçi = message.guild.members.cache.filter(m => !m.user.bot && m.user.presence.status !== "offline").size
     const kobs  = new Discord.MessageEmbed()
-        .setColor("RED")
-        .setThumbnail(`https://media1.tenor.com/images/54cc77830f82ef67471d8d868d09ad2f/tenor.gif?itemid=11230336`)
+        .setColor("#00a5ff")
+        .setThumbnail(`https://cdn.discordapp.com/attachments/762314428045328385/762372336950968330/kobscode.gif`)
         .addField(` **Sunucudaki Toplam Üye Sayısı**`,`**\`\`\`${message.guild.memberCount}\`\`\`**`)
         .addField(` **Toplam Çevrimiçi Üye Sayısı**`, `**\`\`\`${çevrimiçi}\`\`\`**`) 
         .addField(`**Seslideki Üye Sayısı**`,`**\`\`\`${count}\`\`\`**`)
+        .addField(`**Tagdaki Üye Sayısı**`,`**\`\`\`${tagdakiler}\`\`\`**`)
         .addField(` **Kullanıcılar**`, ` Çevrimiçi : **${message.guild.members.cache.filter(o => o.presence.status === 'online').size}** \n Rahatsız Etmeyin : **${message.guild.members.cache.filter(dnd => dnd.presence.status === 'dnd').size}** \n Boşta: **${message.guild.members.cache.filter(i => i.presence.status === 'idle').size}** \n Görünmez/Çevrimdışı : **${message.guild.members.cache.filter(off => off.presence.status === 'offline').size}** \n  Botlar : **${botlar}**`, true)
         .setTitle(`${message.author.tag} - Tarafından Kullanıldı`)
-        .setFooter(`istediğinizi yazın`, client.user.avatarURL())
+        .setFooter(`Kobs Code`, client.user.avatarURL())
     message.channel.send(kobs);
 
 } 
@@ -30,7 +37,7 @@ exports.conf = {
 };
 
 exports.help = {
-    name: 'kobs',
+    name: 'say',
     description: 'Say',
     usage: 'say'
 }
