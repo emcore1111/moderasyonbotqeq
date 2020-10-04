58a7e9d27f88a5b195ec7990468f5a896263cb30
Glitch (v12publicvideokobs) <none>
2020-10-04 18:35:43 +0000

    🌍✌️ Checkpoint ./komutlar/kullanıcı-bilgi.js:14097343/1315 ./komutlar/vip-ver.js:14097343/1724 ./komutlar/vip-al.js:14097343/2411

--- komutlar/kullanıcı-bilgi.js
+++ komutlar/kullanıcı-bilgi.js
@@ -13,9 +13,8 @@ let user;
     }
 
     const member = message.guild.member(user);
-
+       
     const embed = new Discord.MessageEmbed()
-    
         .setColor("GREEN")
     
     .setThumbnail(user.avatarURL)
@@ -25,19 +24,17 @@ let user;
     .addField("İsim :",`${user.username}#${user.discriminator}`, true)
     .addField("İd :", `${user.id}`, true)
     .addField("Discord Tag :", `#${user.discriminator}`, true)
-        .addField("Hesap Oluşturma Tarihi :", `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, ')}`, true)
-        .addField("Sunucuya Katılma Tarihi :", `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY')}`, true)
-        .addField("Durumu :", `${user.presence.status}`, true)
-  
-        .addField("Oynadığı Oyun :", `${user.presence.game ? user.presence.game.name : 'Bilinmiyor'}`, true)
-        
+    .addField("Hesap Oluşturma Tarihi :", `${moment.utc(user.createdAt).format('dddd, MMMM.Do.YYYY, ')}`, true)
+    .addField("Sunucuya Katılma Tarihi :", `${moment.utc(member.joinedAt).format('dddd, MMMM.Do.YYYY')}`, true)
+    .addField("Durumu :", `${user.presence.status}`, true)
+  .setImage('https://cdn.discordapp.com/attachments/762314428045328385/762381656724078652/kobscode_2.gif')
     message.channel.send({embed});
     }
 
 exports.conf = {
   enabled: true, 
   guildOnly: false,
-  aliases: ['profil-bilgi','profilbilgi','kullanıcı-bilgi','kullanıcıbilgi','k-bilgi','kbilgi'], 
+  aliases: ['profil-bilgi','profilbilgi','kullanıcı-bilgi','kullanıcıbilgi','k-bilgi','kbilgi','kb'], 
   permLevel: 0 
 };
 
