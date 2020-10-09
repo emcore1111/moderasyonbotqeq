7ddb900dda7c3989171c82c1bfeb62bd5ac9d668
Glitch (v12publicvideokobs) <none>
2020-10-09 15:02:26 +0000

    👱🍪 Checkpoint ./komutlar/git.js:16128315/544

--- komutlar/git.js
+++ komutlar/git.js
@@ -4,9 +4,19 @@ const db = require('quick.db');
 exports.run = async(client, message, args) => {
    if(!message.member.roles.cache.has('762306051336437800')) return message.channel.send('Bu komutu kullanabilmek için gerekli yetkiye sahip değilsin : `rôl adı`')
 
+  ///OLMSSA SYLE BISEYLER DAHA YAPICAM "!" GİBİ
+  
+  if(!message.member.voiceChannel) return message.channel.send("Sese Gir.")
+  
   let member = message.mentions.members.first()
   
-  if(!member)
+  if(!member) return message.channel.send("Bir Üye Etiketle.")
+  
+  if(member.voiceChannel) return message.channel.send("Etiketlenen Üye Sesde Yok.")
+  
+  if(member.voiceChannel.id = message.member.voiceChannel.id) return message.channel.send("Zaten Aynı Kanaldasınız.")
+  
+  message.member.setVoice
   
 }
 
