9d0eb3b06d463e023ca39e73f72f1ac32a2591c8
Glitch (v12publicvideokobs) <none>
2020-10-09 15:27:26 +0000

    🏆🎾 Checkpoint ./komutlar/git.js:16128315/1211

--- komutlar/git.js
+++ komutlar/git.js
@@ -12,11 +12,41 @@ exports.run = async(client, message, args) => {
   
   if(!member) return message.channel.send("Bir Üye Etiketle.")
   
-  if(member.voiceChannel) return message.channel.send("Etiketlenen Üye Sesde Yok.")
+  if(!member.voiceChannel) return message.channel.send("Etiketlenen Üye Sesde Yok.")
   
   if(member.voiceChannel.id = message.member.voiceChannel.id) return message.channel.send("Zaten Aynı Kanaldasınız.")
   
-  message.member.setVoice
+ let rochelle = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setDescription(
+      `${member} , ${message.author} Senin Yanına Gelmek İstiyor Kabul Ediyormusun.?`
+    );
+  message.channel.send(rochelle).then(async (m) => {
+    await m.react("✅").then((r) => {
+      const tamam = (reaction, user) =>
+        reaction.emoji.name == "✅" && user.id == message.author.id;
+      const tamam2 = m.createReactionCollector(tamam);
+
+      tamam2.on("collect", async (r) => {
+        let embed = new Discord.MessageEmbed()
+          .setColor("0x36393E")
+          .setTitle(" Başarılı")
+          .setDescription(`✅ **|** Başarıyla Odaya Gittin MORUQ.`);
+        message.channel.send(embed);
+        await member.setVoiceChannel(message.member.voiceChannel);
+      });
+    });
+    m.react("❌").then((r) => {
+      const tamam = (reaction, user) =>
+        reaction.emoji.name == "❌" && user.id == message.author.id;
+      const tamam2 = m.createReactionCollector(tamam);
+
+      tamam2.on("collect", async (r) => {
+        m.delete();
+        message.channel.send("Gitme İşlemi İptal Edildi.");
+      });
+    });
+  });
   
 }
 
