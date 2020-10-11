75ba886943aa931f7a4a3ee561f4fd48d335f71e
Glitch (v12publicvideokobs) <none>
2020-10-11 14:57:20 +0000

    🗻🚌 Checkpoint ./komutlar/unban.js:14097343/1101

--- komutlar/unban.js
+++ komutlar/unban.js
@@ -1,20 +1,15 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+  const db = require('quick.db');
 
 exports.run = async (client, message, args) => {
-
-  const db = require('quick.db');
   
-  const kobs = require("../ayarlar/bot.json");
-let prefix = await db.fetch(`prefix.${message.guild.id}`) || kobs.prefix;
     
-  if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`<a:hypesquad1:750076071721828452>  **Bu komutu kullanabilmek için "\`Üyeleri Yasakla\`" yetkisine sahip olmalısın.**`);
+  if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`**Bu komutu kullanabilmek için "\`Üyeleri Yasakla\`" yetkisine sahip olmalısın.**`);
   
 
   let user = args[0];
   let reason = args.slice(1).join(' ');
-  if (db.has(`log_${message.guild.id}`) === false) return message.channel.send(`<a:setting:750076062716788807> **Mod Log Kanalı Ayarlanmamış | ${prefix}modlog #kanal**`);
-  let modlog = message.guild.channels.cache.get(db.fetch(`log_${message.guild.id}`).replace("<#", "").replace(">", ""));
  if (isNaN(user)) return message.channel.send('**Lütfen Banını Açmak İstediğiniz Üyeninin ID sini Girin**');
   if (reason.length < 1) return message.channel.send('**Lütfen Sebep Giriniz**');
  
@@ -22,10 +17,10 @@ let prefix = await db.fetch(`prefix.${message.guild.id}`) || kobs.prefix;
   const embed = new Discord.MessageEmbed()
   .setColor("#ffd100")
   .addField('İşlem', 'Ban Kaldırma')
-  .addField('Banı Açılan Üye', `(${user})`)
-  .addField('Banı Açan Yetkili', `${message.author.username}#${message.author.discriminator}`)
+  .addField('Banı Açılan Üye', `<@${user}>`)
+  .addField('Banı Açan Yetkili', `<@${message.author.username}#${message.author.discriminator}>`)
   .addField('Banı Açma Sebebi', "```" + reason + "```")
-  modlog.send(embed);
+  client.channels.cache.get('764861681288609795').send(embed)///LOG KANAL İD YAZMALISIN
   message.guild.members.unban(user);
   
 
@@ -37,8 +32,15 @@ let prefix = await db.fetch(`prefix.${message.guild.id}`) || kobs.prefix;
 
   
 };
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['unban','ban-kaldır'],
+    permLevel: 0
+};
 
-exports.config = {
-  name: 'unban',
-  aliases: ['unban','yasak-kaldır','yasak-aç','ban-kaldır']
+exports.help = {
+    name: 'unban',
+    description: 'unban',
+    usage: 'unban'
 };
