9324605a405e414203405d23331949f94492b444
Glitch (v12publicvideokobs) <none>
2020-10-11 12:28:37 +0000

    🚥🐕 Checkpoint ./komutlar/yardım.js:14097343/3494

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,32 +1,49 @@
-const Discord = require('discord.js'); 
- 
-exports.run = async(client, message, args) => {
- 
- const yardım = new Discord.MessageEmbed()
- .setTimestamp()
- .setTitle("Yardım Menusu")
-.setColor("BLUE")
- .addField("Kayıt Komutlar(0)", `
-Kayıt Sisteminde Kullanılan Komutlar;
-\`kız\`,\`erkek\`,\`nick\`
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json')
+exports.run = function(client, message) {
+  
+  let prefix  = ayarlar.prefix
+
+  
+const yardım = new Discord.MessageEmbed()
+.setColor('RANDOM')
+.setTitle(`${client.user.username} - Komutlarım`)
+.setDescription(`
+**\`Kayıt\`**
+
+» \`${prefix}erkek/kız <isim> <yas>\`: **Belirtilen Kullanıcıyı Kayıt Edersiniz**
+» \`${prefix}nick <isim> <yas>\`: **Belirtilen Kullanıcının İsmini Değiştirirsiniz**
+» \`${prefix}vip al/ver\`: **Kullanıcıya Vip Al/Ver**
+» \`${prefix}kullanıcı-bilgi\`: **Etiketlediğin / kendi profilin hakkında bilgi verir. Vip Al/Ver**
+
+**\`Yetkili\`**
+
+» \`${prefix}ban <kullanıcı> <sebep>\`: **Belirtilen Kullanıcıyı Sunucudan Yasaklar**
+» \`${prefix}kick <kullanıcı> <sebep>\`: **Belirtilen Kullanıcıyı Sunucudan Atar**
+» \`${prefix}mute <kullanıcı> <süre> <sebep>\`: **Kullanıcıyı Belirtilen Süre Boyunca Susturur**
+» \`${prefix}unban <id>\`: **Belirtilen İD Deki Kişinin Sunucudan Yasağını Kaldırır**
+» \`${prefix}temizle/sil <0-100>\`: **Belirtilen Miktarda Mesajı Siler**
+
+
 `)
- .addField("-eğlence (9)", `
-Yetkili Komutları;
-\`ban\`,\`mute\`,\`sohbet-aç\`,\`sohbet-kapat\`,\`kediol\`,\`aşk\`,\`token\`,\`tokat\`
-`)
- 
- message.channel.send(yardım)
-}
+
+.setThumbnail(message.author.avatarURL({dynamic: true}))
+.setTimestamp()
+message.channel.send(yardım)
+  
+   
+  
+};
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ["yardım","help","help"],
+  guildOnly: false, 
+  aliases: [], 
   permLevel: 0
 };
 
 exports.help = {
-  name: 'yardım',
-  description: 'Yardım Menüsünü Gösterir.',
+  name: "yardım",
+  description: 'yardım kodu.',
   usage: 'yardım'
-}
+};
